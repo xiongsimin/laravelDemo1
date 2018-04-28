197f99c846afa89f9760bd82927320da79045d3b
xiongsimin <2248950919@qq.com>
2018-04-28 23:41:16 +0800

    xiongsimin

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
 
-	protected $fillable=['title','content','intro'];
+	protected $fillable=['title','content','intro','published_at'];
 
 }
--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,6 +10,9 @@
         {!!Form::label('内容')!!}
         {!!Form::textarea('content',null,['class'=>'form-control'])!!}
     </div>
+    <div class="form-group">
+        {!!Form::input('date','published_at',date('Y-m-d'),['class'=>'form-control'])!!}
+    </div>
     {!!Form::submit('发表文章',['class'=>'btn btn-primary form-control'])!!}
     {!!Form::close()!!}
 @stop
