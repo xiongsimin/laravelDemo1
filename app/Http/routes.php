9d148b05fea70fdf743717d7da499913d8dcf012
xiongsimin <2248950919@qq.com>
2018-04-29 11:21:25 +0800

    xiongsimin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,7 @@ Route::get('/articles','ArticlesController@index');
 Route::get('/articles/create','ArticlesController@create');
 Route::get('/articles/{id}','ArticlesController@show');
 Route::post('/articles','ArticlesController@store');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
